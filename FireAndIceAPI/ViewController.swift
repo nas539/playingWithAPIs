99c65dec5b1ea84ca6348a51f9d561b0ac8e156c
Nicholas Sharp <naas8905@gmail.com>
2020-06-03 15:48:45 -0700

    Initial commit

---
+++ FireAndIceAPI/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FireAndIceAPI
+//
+//  Created by Nicholas Sharp on 4/16/19.
+//  
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
